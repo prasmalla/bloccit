92ade8b714a44ab4644627de9ca1a38f7c9a698d
prasmalla <pras@kaufmann-mercantile.com>
2014-12-07 18:40:17 -0500

    add shallow nested comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
+    @topic = @post.topic
     @comment = Comment.new(params[:comment])
     @comment.post = @post
     @comment.user = current_user
@@ -16,8 +16,8 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @topic = @post.topic
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
