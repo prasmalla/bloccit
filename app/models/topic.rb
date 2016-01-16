51626eaaa8e40fd55929ff3283da2884a2e681f6
prasmalla <pras@kaufmann-mercantile.com>
2016-01-15 21:35:03 -0500

    checkpoint 53 complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   attr_accessible :description, :name, :public
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
-
+  # scope :visible_to, -> (user) { user ? all : where(public: true) } RAILS4 all returns AR relation
+  scope :visible_to, -> (user) { user ? scoped : where(public: true) } #rails3 use scoped
   paginates_per 10
 end
