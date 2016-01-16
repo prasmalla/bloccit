6b0115e1a7241619dc0bf803daa534e58654ad40
prasmalla <pras@kaufmann-mercantile.com>
2016-01-16 00:39:15 -0500

    assignment 53 completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
   end 
+
+  def up_vote_link_classes(post)
+    current_user.voted(post) && current_user.voted(post).up_vote? ? 'voted' : ''
+  end
+
+  def down_vote_link_classes(post)
+    current_user.voted(post) && current_user.voted(post).down_vote? ? 'voted' : ''
+  end
 end
