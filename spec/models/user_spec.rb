3f2f5cd1959897c59d4073a2cad2421c5230dedc
prasmalla <pras@kaufmann-mercantile.com>
2016-01-18 16:24:15 -0500

    assignment 55 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,9 +21,7 @@
 
   describe ".top_rated" do 
     before do
-      @user1 = create(:user)
-      post = create(:post, user: @user1)
-      create(:comment, user: @user1, post: post)
+      @user1 = create(:user_with_post_and_comment)
 
       @user2 = create(:user)
       post = create(:post, user: @user2)
